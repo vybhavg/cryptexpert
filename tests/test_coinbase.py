0aca7803061095a034a7ad5c95c474a3ef76725d
vybhav <126269634+vybhavg@users.noreply.github.com>
2025-03-17 00:27:07 +0530

    Update test_coinbase.py

--- tests/test_coinbase.py
+++ tests/test_coinbase.py
@@ -4,7 +4,7 @@ from unittest.mock import patch, Mock
 from mark.routes import get_specific_prices_from_coinbase  # Import from app.py
 
 class TestCoinbasePrices(unittest.TestCase):
-    @patch('cryptexpert.app.requests.get')
+    @patch('mark.routes.requests.get')
     def test_get_specific_prices_from_coinbase(self, mock_get):
         # Mock the API response
         mock_response = Mock()
