2c668f1d668114f925052706c2df6c930643bf69
vybhav <126269634+vybhavg@users.noreply.github.com>
2025-03-17 01:05:30 +0530

    Update locustfile.py

--- tests/locustfile.py
+++ tests/locustfile.py
@@ -1,20 +1,16 @@
 from locust import HttpUser, task, between
 
-class CryptexpertUser(HttpUser):
-    wait_time = between(1, 5)  # Simulated users will wait between 1 and 5 seconds between tasks
-
-   
-    @task
-    def live_prices(self):
-        # Simulate a request to the live prices page
-        self.client.get("/live_prices")
-
-
-
+class CryptoPriceUser(HttpUser):
+    wait_time = between(1, 3)  # Simulate users waiting before making another request
 
     @task
-    def ai_predictor(self):
-        # Simulate a request to the AI predictor page
-        self.client.get("/ai_predictor")
+    def get_binance_prices(self):
+        self.client.get("/binance_prices")
 
-  
+    @task
+    def get_okx_prices(self):
+        self.client.get("/okx_prices")
+
+    @task
+    def get_coinbase_prices(self):
+        self.client.get("/coinbase_prices")
