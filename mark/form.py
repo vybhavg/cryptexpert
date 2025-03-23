440dab4e602db07617a5c1c6e860de401142828c
vybhav <126269634+vybhavg@users.noreply.github.com>
2025-03-23 17:50:27 +0530

    Update form.py

--- mark/form.py
+++ mark/form.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField,PasswordField,SubmitField
-from wtforms.validators import Length, EqualTo, Email,DataRequired,ValidationError
+from wtforms.validators import Length, EqualTo, Email,DataRequired,ValidationError,TextAreaField
 from mark.models import User, UserAPIKey
 
 class RegisterForm(FlaskForm):
