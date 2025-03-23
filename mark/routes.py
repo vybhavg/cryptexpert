94f6bde2146781c581b98b8a9d03866a90fcfdea
vybhav <126269634+vybhavg@users.noreply.github.com>
2025-03-23 18:04:59 +0530

    Update routes.py

--- mark/routes.py
+++ mark/routes.py
@@ -1,5 +1,5 @@
 from mark import app, db, mail
-from mark.form import RegisterForm, LoginForm, otpform, verifyform,Authenticationform
+from mark.form import RegisterForm, LoginForm, otpform, verifyform,Authenticationform,ThreadForm,PostForm
 from mark.models import User, Item, CryptoAsset, Exchange, UserAPIKey
 from flask import render_template,request, redirect, url_for, flash, session
 from flask_login import login_user, logout_user, login_required, current_user
