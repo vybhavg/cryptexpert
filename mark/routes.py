62199d8e0957bf617c05fd447bfbf27337da2eea
vybhav <126269634+vybhavg@users.noreply.github.com>
2025-04-12 19:48:03 +0530

    Update routes.py

--- mark/routes.py
+++ mark/routes.py
@@ -574,7 +574,7 @@ def charts():
 
 # Load the trained model
 # Load the trained model
-model = load_model("/app/mark/model.keras")
+model = load_model("/home/ec2-user/cryptexpert/mark/model.keras")
 
 
 
