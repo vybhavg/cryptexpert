64931a11d6c089362cd46fd13bc8811d30da0859
vybhav <126269634+vybhavg@users.noreply.github.com>
2025-04-12 20:08:53 +0530

    Update routes.py

--- mark/routes.py
+++ mark/routes.py
@@ -251,20 +251,7 @@ def get_specific_prices_from_coinbase():
 @app.route('/')
 @app.route('/home')
 def home():
-    binance_prices = get_specific_prices_from_binance()
-    okx_prices = get_specific_prices_from_okx()
-    coinbase_prices = get_specific_prices_from_coinbase()
-
-    for ticker in binance_prices:
-        ticker['priceChangePercent'] = float(ticker['priceChangePercent'])
-
-    for ticker in okx_prices:
-        ticker['priceChangePercent'] = float(ticker['priceChangePercent'])
-
-    for ticker in coinbase_prices:
-        ticker['priceChangePercent'] = float(ticker['priceChangePercent'])
-
-    return render_template('index.html', binance_prices=binance_prices, okx_prices=okx_prices, coinbase_prices=coinbase_prices,crypto_logos=crypto_logos)
+    return render_template('index.html',crypto_logos=crypto_logos)
 
 
 @app.route('/binance_prices')
