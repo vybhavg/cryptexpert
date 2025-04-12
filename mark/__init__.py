329673baa6d2046be541594232f7e3f062d18993
vybhav <126269634+vybhavg@users.noreply.github.com>
2025-04-12 18:57:51 +0530

    Update __init__.py

--- mark/__init__.py
+++ mark/__init__.py
@@ -5,8 +5,7 @@ from flask_login import LoginManager
 from flask_mail import Mail
 from flask_migrate import Migrate
 import os
-import urllib.parse  # Add this import
-from dotenv import load_dotenv
+from dotenv import load_dotenv  # Import dotenv to load .env file
 
 # Load environment variables from the .env file
 load_dotenv()
@@ -15,7 +14,7 @@ app = Flask(__name__)
 
 # Get environment variables
 db_user = os.getenv("DB_USER", "postgres")
-db_password = os.getenv("DB_PASSWORD", "Gvbh1781")  # Use env var in production
+db_password = os.getenv("DB_PASSWORD", "Gvbh1781")  # Default for local, use env var in production
 db_name = os.getenv("DB_NAME", "cryptexpert")
 db_host = os.getenv("DB_HOST", "cryptexpert-db.cjs6a2mg6ff2.ap-southeast-1.rds.amazonaws.com")
 db_port = os.getenv("DB_PORT", 5432)
@@ -28,15 +27,15 @@ app.config["SQLALCHEMY_DATABASE_URI"] = (
     f"postgresql+psycopg2://{db_user}:{encoded_password}@{db_host}:{db_port}/{db_name}"
 )
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', os.urandom(24))  # Load secret key from env
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', os.urandom(24))  # Use env var for secret key
 
 # Flask-Mail Configuration
-app.config['MAIL_SERVER'] = 'smtp.gmail.com'
-app.config['MAIL_PORT'] = 587
-app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_SERVER'] = os.getenv('MAIL_SERVER', 'smtp.gmail.com')
+app.config['MAIL_PORT'] = int(os.getenv('MAIL_PORT', 587))
+app.config['MAIL_USE_TLS'] = os.getenv('MAIL_USE_TLS', 'True') == 'True'
 app.config['MAIL_USERNAME'] = os.getenv('MAIL_USERNAME', 'cryptexpert.v@gmail.com')
 app.config['MAIL_PASSWORD'] = os.getenv('MAIL_PASSWORD', 'ujreujuqioixlqga')
-app.config['MAIL_DEFAULT_SENDER'] = os.getenv('MAIL_USERNAME', 'cryptexpert.v@gmail.com')
+app.config['MAIL_DEFAULT_SENDER'] = os.getenv('MAIL_DEFAULT_SENDER', 'cryptexpert.v@gmail.com')
 
 # Initializing Extensions
 mail = Mail(app)
