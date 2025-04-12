6575367455ecdbe7f5cbbb03dd6a51d8b13a6d9e
vybhav <126269634+vybhavg@users.noreply.github.com>
2025-04-12 23:07:45 +0530

    Update routes.py

--- mark/routes.py
+++ mark/routes.py
@@ -251,8 +251,21 @@ def get_specific_prices_from_coinbase():
 @app.route('/')
 @app.route('/home')
 def home():
-    dummy_ticker = {'symbol': 'BTCUSDT','price': '0.00','priceChangePercent': 0}
-    return render_template('index.html',binance_prices=[dummy_ticker]*80 ,okx_prices=[dummy_ticker]*80,coinbase_prices=[dummy_ticker]*80,crypto_logos=crypto_logos)
+    binance_prices = get_specific_prices_from_binance()
+    okx_prices = get_specific_prices_from_okx()
+    coinbase_prices = get_specific_prices_from_coinbase()
+
+    for ticker in binance_prices:
+        ticker['priceChangePercent'] = float(ticker['priceChangePercent'])
+
+    for ticker in okx_prices:
+        ticker['priceChangePercent'] = float(ticker['priceChangePercent'])
+
+    for ticker in coinbase_prices:
+        ticker['priceChangePercent'] = float(ticker['priceChangePercent'])
+
+    return render_template('index.html', binance_prices=binance_prices, okx_prices=okx_prices, coinbase_prices=coinbase_prices,crypto_logos=crypto_logos)
+
 
 @app.route('/binance_prices')
 def binance_prices():
@@ -1202,4 +1215,4 @@ def create_post(thread_id):
         db.session.add(post)
         db.session.commit()
         flash('Post created successfully!', 'success')
-    return redirect(url_for('forum_thread', thread_id=thread_id))
+    return redirect(url_for('forum_thread', thread_id=thread_id)
