56f320e03863749238a99e0fc5439370dc9a14e4
vybhav <126269634+vybhavg@users.noreply.github.com>
2025-04-12 19:00:04 +0530

    Update __init__.py

--- mark/__init__.py
+++ mark/__init__.py
@@ -4,6 +4,7 @@ from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_mail import Mail
 from flask_migrate import Migrate
+import urllib.parse
 import os
 from dotenv import load_dotenv  # Import dotenv to load .env file
 
@@ -18,24 +19,21 @@ db_password = os.getenv("DB_PASSWORD", "Gvbh1781")  # Default for local, use env
 db_name = os.getenv("DB_NAME", "cryptexpert")
 db_host = os.getenv("DB_HOST", "cryptexpert-db.cjs6a2mg6ff2.ap-southeast-1.rds.amazonaws.com")
 db_port = os.getenv("DB_PORT", 5432)
-
-# Safely encode special characters in password
+# Safely encode special characters
 encoded_password = urllib.parse.quote_plus(db_password)
 
-# Setup the SQLAlchemy database URI
 app.config["SQLALCHEMY_DATABASE_URI"] = (
     f"postgresql+psycopg2://{db_user}:{encoded_password}@{db_host}:{db_port}/{db_name}"
 )
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', os.urandom(24))  # Use env var for secret key
-
 # Flask-Mail Configuration
-app.config['MAIL_SERVER'] = os.getenv('MAIL_SERVER', 'smtp.gmail.com')
-app.config['MAIL_PORT'] = int(os.getenv('MAIL_PORT', 587))
-app.config['MAIL_USE_TLS'] = os.getenv('MAIL_USE_TLS', 'True') == 'True'
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USERNAME'] = os.getenv('MAIL_USERNAME', 'cryptexpert.v@gmail.com')
 app.config['MAIL_PASSWORD'] = os.getenv('MAIL_PASSWORD', 'ujreujuqioixlqga')
-app.config['MAIL_DEFAULT_SENDER'] = os.getenv('MAIL_DEFAULT_SENDER', 'cryptexpert.v@gmail.com')
+app.config['MAIL_DEFAULT_SENDER'] = 'cryptexpert.v@gmail.com'
 
 # Initializing Extensions
 mail = Mail(app)
@@ -44,5 +42,3 @@ bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login_form'
 migrate = Migrate(app, db)  # Ensure this line is included
-
-from mark import routes
