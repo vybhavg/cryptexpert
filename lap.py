d965bae4a93724c96e821826bf07af7757082baa
vybhav <126269634+vybhavg@users.noreply.github.com>
2025-04-17 23:26:32 +0530

    Update lap.py

--- lap.py
+++ lap.py
@@ -3,4 +3,6 @@ import os
 os.environ['CUDA_VISIBLE_DEVICES'] = '-1'  # Disable GPU
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'  # Suppress TensorFlow logs
 if(__name__=="__main__"):
-    app.run(host="0.0.0.0", port=8080, debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Render sets PORT env variable
+    
+    app.run(host='0.0.0.0', port=port)
