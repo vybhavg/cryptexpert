f3f314b28e43ce881086fc619df6292833085652
vybhav <126269634+vybhavg@users.noreply.github.com>
2025-04-19 23:07:11 +0530

    Update lap.py

--- lap.py
+++ lap.py
@@ -1,6 +1,6 @@
-from mark import app
+from mark import app,socketio
 import os
 os.environ['CUDA_VISIBLE_DEVICES'] = '-1'  # Disable GPU
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'  # Suppress TensorFlow logs
 if(__name__=="__main__"):
-    app.run(host="0.0.0.0", port=8080, debug=True)
+    socketio.run(host="0.0.0.0", port=8080, debug=True)
