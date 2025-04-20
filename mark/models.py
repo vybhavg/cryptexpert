5b0d62b56f9f00320f6050d30871873d571b78ea
vybhav <126269634+vybhavg@users.noreply.github.com>
2025-04-21 00:46:23 +0530

    Update models.py

--- mark/models.py
+++ mark/models.py
@@ -152,6 +152,7 @@ class Notification(db.Model):
 
     def __repr__(self):
         return f'<Notification {self.id} for user {self.user_id}>'
+
 class BlogCategory(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), unique=True, nullable=False)
