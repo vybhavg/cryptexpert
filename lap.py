ed4fcf98f9b376644ca4e484854605e9f3631040
vybhav <126269634+vybhavg@users.noreply.github.com>
2025-08-16 20:07:25 +0530

    Update lap.py

--- lap.py
+++ lap.py
@@ -1,11 +1,9 @@
-import os
 from mark import app, socketio
+import os
 
-# Disable GPU and suppress TensorFlow logs (as in your original code)
-os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+os.environ['CUDA_VISIBLE_DEVICES'] = '-1'  # Disable GPU
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'  # Suppress TensorFlow logs
 
 if __name__ == "__main__":
-    # Get the port from the environment variable, default to 5000 if not set (for local testing)
     port = int(os.environ.get("PORT", 5000))
-    socketio.run(app, host="0.0.0.0", port=port, debug=True)
+    socketio.run(app, host="0.0.0.0", port=port, debug=True, allow_unsafe_werkzeug=True)
